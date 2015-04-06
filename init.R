4556cea547ef23d7bd6eee50b625d54b6a990bf7
DanRunfola <dan@danrunfola.com>
2015-04-06 13:58:51 -0400

    Updating Descriptives

--- init.R
+++ init.R
@@ -2,23 +2,23 @@
 #To test sub-modules of C-SAT
 
 #Load required libraries from our dependencies script.
-#source('Dependencies/dep.R', chdir=T)
+source('Dependencies/dep.R', chdir=T)
 
 #Load user input settings
-#source('user_inputs.R', chdir=T)
+source('user_inputs.R', chdir=T)
 
 #Load descriptive tools
-#source('Tools/descriptives.R', chdir=T)
+source('Tools/descriptives.R', chdir=T)
 
 #Custom Functions
-#source('functions.R', chdir=T)
+source('functions.R', chdir=T)
 
 #Later tool loads will go here - closeness, matching, balancing, modeling.
 
 #--------------------------------
 #Tool runs
-runApp(".")
+#runApp(".")
 
-#descTrtCont(shp=user_shape, flds=(c("Accepted_Y","NDVI_","UF","pop_FUNAI2"),c("con","time","factor","con")), method=c("thresh","manual",2002,"<"))
+descTrtCont(shp=user_shape, flds=(c("Accepted_Y","NDVI_","UF","pop_FUNAI2"),c("con","time","factor","con")), method=c("thresh","manual",2002,"<"))
 
 #psmAsumTests()

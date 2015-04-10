650316449838ec59219f62da50bea8603202a494
DanRunfola <dan@danrunfola.com>
2015-04-10 09:06:19 -0400

    Dist challenge for scott

--- Tools/SpatialCausalPSM.R
+++ Tools/SpatialCausalPSM.R
@@ -14,7 +14,7 @@ SpatialCausalPSM <- function(dta, mtd,mdl,drop)
   treated$trt = 'treated'
   untreated$trt = 'untreated'
   trtLen <- rbind(treated,untreated)
-  print(ggplot(trtLen, aes(PSM_trtProb, fill=trt)) + geom_density(alpha=0.2) + ggtitle("Trt/Cont PSM Values"))
+  print(ggplot(trtLen, aes(PSM_trtProb, fill=trt)) + geom_density(alpha=0.3) + ggtitle("Trt/Cont PSM Values"))
   
   #Second, if a drop parameter - if set to "overlap", remove observations
   #that don't overlap in the PSM distribution.
@@ -25,8 +25,8 @@ SpatialCausalPSM <- function(dta, mtd,mdl,drop)
     min_cut <- max(min(treated$PSM_trtProb), min(untreated$PSM_trtProb))
     max_cut <- min(max(treated$PSM_trtProb), max(untreated$PSM_trtProb))
     
-    retData@data <- retData@data[retData@data$PSM_trtProb >= min_cut,]    
-    retData@data <- retData@data[retData@data$PSM_trtProb <= max_cut,] 
+    retData <- retData[retData@data$PSM_trtProb >= min_cut,]    
+    retData <- retData[retData@data$PSM_trtProb <= max_cut,] 
     
     #Post drop histograms
     treated <- retData@data[retData@data$TrtBin == 1,]
@@ -34,7 +34,7 @@ SpatialCausalPSM <- function(dta, mtd,mdl,drop)
     treated$trt = 'treated'
     untreated$trt = 'untreated'
     trtLen <- rbind(treated,untreated)
-    print(ggplot(trtLen, aes(PSM_trtProb, fill=trt)) + geom_density(alpha=0.2) + ggtitle("Post-Extrapolation Drops"))
+    print(ggplot(trtLen, aes(PSM_trtProb, fill=trt)) + geom_density(alpha=0.3) + ggtitle("Post-Extrapolation Drops"))
   }
   
   return (retData)
@@ -42,7 +42,16 @@ SpatialCausalPSM <- function(dta, mtd,mdl,drop)
 
 SpatialCausalDist <- function(dta, mtd)
 {
-  if (mtd = "optNN")
+  #Plot the pre-dropping balance on controls used for PSM model...
+  
+  if (mtd == "fastNN")
+  {
+    #Fast nearest neighbors search - will not arrive at optimum,
+    #but this may not be an issue for many analysis.
+    #-------Need to add in the non-optimal NN loop here, rank by PSM (highest goes first).
+    print("non-opt")
+  }
+  if (mtd == "optNN")
   {
     print("To contain a nearest-neighbor algorithm...")
   }

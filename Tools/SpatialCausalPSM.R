c2f4c91f8183619e776075e843f7122fc69c5129
DanRunfola <dan@danrunfola.com>
2015-04-15 08:17:52 -0400

    Simulations - first run, enforced corr.

--- Tools/SpatialCausalPSM.R
+++ Tools/SpatialCausalPSM.R
@@ -179,32 +179,54 @@ SpatialCausalDist <- function(dta, mtd, vars, ids, drop_unmatched, drop_method, 
 }
 
 
-SpatialCausalSim_DGP<- function(fld_size)
+SpatialCausalSim_DGP<- function(fld_size, rho_opt,rho_mult)
 {
   x <- fld_size
   RFoptions(seed=NA)
-  model <- RMexp()
-  z <- RFsimulate(model, x, x, n=5)
-  f.SPDF <- as(z, 'SpatialPolygonsDataFrame')
   
+  model <- RMexp()
+  z <- RFsimulate(model, x, x, n=6)
+  f.SPDF <- as(z, 'SpatialPolygonsDataFrame')
   
   colnames(f.SPDF@data)[1] <- "ControlA"
   colnames(f.SPDF@data)[2] <- "RandomFieldA"
   colnames(f.SPDF@data)[3] <- "ControlB"
   colnames(f.SPDF@data)[4] <- "RandomFieldB"
   colnames(f.SPDF@data)[5] <- "TrtBin"
+  colnames(f.SPDF@data)[6] <- "tmpLag"
+  
+  #Amplify spatial correlation to an arbitrary degree in each dataset.
+  #This is to account for the issue that gaussian random fields are highly unlikely to have
+  #Extreme values of spatial correlation.
+  f.NB = poly2nb(f.SPDF)
+  f.W = nb2listw(f.NB, style='W')
+
+  for (i in 1:5)
+  {
+    if(grepl(colnames(f.SPDF@data)[i],rho_opt) == 1)
+    {
+      rho = runif(1,0.1,(1*rho_mult))
+      ev_st = paste("eleLag <- lag.listw(f.W, f.SPDF@data[[",i,"]])",sep="")
+      eval(parse(text=ev_st))
+      f.SPDF@data["tmpLag"]=data.frame(eleLag)
+      ev_stB = paste("f.SPDF@data[",i,"] = (f.SPDF@data['tmpLag']*rho) + f.SPDF@data[",i,"]")
+      eval(parse(text=ev_stB))
+    }
+    
+  }
   
   f.SPDF@data["TrtBin"] = f.SPDF@data["ControlA"] + f.SPDF@data["RandomFieldA"]
-  
   #Convert the Treatment to a Binary
-  med_treat = median(f.SPDF@data$TrtBin)
-  f.SPDF@data$TrtBin[which(f.SPDF@data$TrtBin > med_treat )] <- 1 
-  f.SPDF@data$TrtBin[which(f.SPDF@data$TrtBin == med_treat )] <- 1 
-  f.SPDF@data$TrtBin[which(f.SPDF@data$TrtBin < med_treat )] <- 0 
+  #Create a temporary field
+  f.SPDF@data$TrtTemp <- f.SPDF@data$TrtBin
+  med_treat = median(f.SPDF@data$TrtTemp)
+  f.SPDF@data$TrtBin[which(f.SPDF@data$TrtTemp >= med_treat )] <- 1 
+  f.SPDF@data$TrtBin[which(f.SPDF@data$TrtTemp < med_treat )] <- 0 
+  f.SPDF@data <- f.SPDF@data[,!names(f.SPDF) %in% c("TrtTemp")]
   
   #Build outcome variable for testing
   #y = intercept + (Theta * Treatment) + (Beta * ControlA)
-  yfunc <- function(a, b, c) (0.0+(1.0*a)+(1.0*b)+(1.0*c))
+  yfunc <- function(a, b, c) (0.0+(1.0*a)+(1.0*b)+abs(1.0*c))
   f.SPDF@data["y"] = apply(f.SPDF@data[,c('TrtBin','ControlA','RandomFieldB')], 1, function(y) yfunc(y['TrtBin'],y['ControlA'],y['RandomFieldB']))
 
   f.SPDF@data <- cbind(simIDs = rownames(f.SPDF@data), f.SPDF@data)

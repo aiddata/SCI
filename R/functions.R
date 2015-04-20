3a6deab5c5d05efc299f3640bb4a471534348399
DanRunfola <dan@danrunfola.com>
2015-04-20 12:01:12 -0400

    trtLen

--- R/functions.R
+++ R/functions.R
@@ -19,9 +19,9 @@ GroupCompHist = function (dta, compvar, title_pre)
   untreated$trt = 'untreated'
   trtLen <- rbind(treated,untreated)
   ttl = paste(title_pre," (", compvar,")", sep="")
-  bldstr = paste("ggplot(data=trtLen, aes(x=",compvar,",fill=trt)) + geom_density(alpha=0.2)  + ggtitle(ttl)", sep="")
+  print(names(trtLen))
+  bldstr = paste("ggplot(data=trtLen, aes(x=",compvar,",fill=trt)) + geom_density(alpha=0.2, aes(y=..count..))  + ggtitle(ttl)", sep="")
   #geom_bar defaults to stacking.
-  View(trtLen)
 
   print("bldstr and bld")
   print(bldstr)

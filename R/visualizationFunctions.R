e3e18f40d43af1382b5ec4dcd6f06dca55b55009
DanRunfola <dan@danrunfola.com>
2015-04-24 13:01:12 -0400

    Test output

--- R/visualizationFunctions.R
+++ R/visualizationFunctions.R
@@ -28,5 +28,5 @@ ViewTimeSeries <- function(dta,IDfield,TrtField,idPre)
     geom_line(size=.5, linetype=3) + 
     stat_summary(fun.y=mean,aes(x=variable, y=value, group=",TrtField,",colour=factor(",TrtField,")),data=vizDF,geom='line',size=1.5) +
     theme(axis.text.x=element_text(angle=90,hjust=1))",sep="")
-  #print(eval(parse(text=ggplot_exec)))
+  print(eval(parse(text=ggplot_exec)))
 }

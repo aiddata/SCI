62bce5a2cb7e1e56e78486ad70698289f1e475db
DanRunfola <dan@danrunfola.com>
2015-04-24 15:30:23 -0400

    Better return values

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -18,7 +18,7 @@ timeRangeAvg <- function(dta,prefix,startyr,endyr)
   return(rmean)
 }
 
-timeRangeTrend <- function(dta,prefix,startyr,endyr,IDfield,newfieldID)
+timeRangeTrend <- function(dta,prefix,startyr,endyr,IDfield)
 {
   grep_str = paste(IDfield,prefix,sep="|")
   tDF <- dta@data[grepl(grep_str,names(dta@data))]
@@ -27,18 +27,17 @@ timeRangeTrend <- function(dta,prefix,startyr,endyr,IDfield,newfieldID)
   #cleaned GREP
   new_pre <- gsub("[0-9]","",prefix,fixed=TRUE)
   analysisDF["Year"] <- lapply(analysisDF["variable"],FUN=function(x) as.numeric(gsub(new_pre,"",x)))
-  View(analysisDF)
   analysisDF <- analysisDF[analysisDF["Year"] >= startyr ,]
   analysisDF <- analysisDF[analysisDF["Year"] <= endyr ,]
-  dta@data[newfieldID] <- 0
+  dta@data["newfieldID"] <- 0
   for (i in 1:length(dta))
   {
     ID <- as.character(dta@data[IDfield][i,])
     #Fit trend model
     ID_dat <- analysisDF[analysisDF[IDfield] == ID,]
     trend_mod <- lm(value ~ Year,data=ID_dat)
-    dta@data[newfieldID][i,] <- summary(trend_mod)$coefficients[2]
+    dta@data["newfieldID"][i,] <- summary(trend_mod)$coefficients[2]
   }
-  return(dta)
+  return(dta$newfieldID)
   
 }

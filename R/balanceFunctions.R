66c592ecef5fcb421003f907254880870b611d2c
DanRunfola <dan@danrunfola.com>
2015-04-28 15:22:49 -0400

    Kilometers rather than meters in the call, more clarifty in functional specifications

--- R/balanceFunctions.R
+++ R/balanceFunctions.R
@@ -35,15 +35,18 @@ GroupCompHist = function (dta, compvar, title_pre,simple_out)
 #PSM distance decay examination - should we enforce a threshold for matches or not?
 #Need to provide better settings for the distance bands, hacked for now.
 #Order also needs to be examined
-PSMdistDecay = function(dta,psm_col,c1,c2,h)
+PSMdistDecay = function(dta,psm_col,start_bin_dist,increment_dist,h)
 {
   #Produce a corellogram using Moran's I at varying resolutions
   #First, convert to an equal-distance projection
+  
   dta_prj_coords <- project(as.matrix(coordinates(dta)),"+proj=laea") 
   dta_prj <- as(dta,"data.frame")
   coordinates(dta_prj) <- dta_prj_coords
+  c1 = start_bin_dist * 1000
+  c2 = increment_dist * 1000
   r.nb <- dnearneigh(as.matrix(coordinates(dta_prj)),d1=c1,d2=c2)
-
+  
   exec <- paste("sp.correlogram(r.nb,dta_prj$",psm_col,",order=",h,",method='I',zero.policy=TRUE)",sep="")
   sp.cor <- eval(parse(text=exec))
   plot(sp.cor)

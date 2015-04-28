f22f65d62072c3d82b55802581cb4a7f67582a04
DanRunfola <dan@danrunfola.com>
2015-04-28 17:16:55 -0400

    test

--- R/PSM_correlogram.R
+++ R/PSM_correlogram.R
@@ -19,7 +19,6 @@ PSM_correlogram <- function (neighbours, var, order = 1, style = "W",
     stop("order less than 1")
   nblags <- nblag(neighbours, maxlag = order)
   cardnos <- vector(mode = "list", length = order)
-  print(cardnos)
   for (i in 1:order) cardnos[[i]] <- table(card(nblags[[i]]))
   nobs <- sapply(cardnos, function(x) sum(x[names(x) > "0"]))
   #if (any(nobs < 3)) 
@@ -46,6 +45,8 @@ PSM_correlogram <- function (neighbours, var, order = 1, style = "W",
       res <- res[-k]
     }
   }
+  
+  print(cardnos)
   
   obj <- list(res = res, method = "I", cardnos = cardnos, 
               var = deparse(substitute(var)))

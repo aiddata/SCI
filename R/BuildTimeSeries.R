0f7096b72f4f3280ba448451a9efbb609d1cce55
DanRunfola <dan@danrunfola.com>
2015-05-05 15:21:44 -0400

    Including spatial analyses

---
+++ R/BuildTimeSeries.R
@@ -0,0 +1,12 @@
+BuildTimeSeries <- function(data,idField,varList_pre)
+{
+  grep_str = idField
+  for (i in 1:length(varList_pre))
+  {
+    grep_str = paste(grep_str,varList_pre[i],sep="|")
+  }
+
+  tDF <- dta@data[grepl(grep_str,names(dta@data))]
+  retDF <- melt(tDF,id=c(idField))
+  return(retDF)
+}
--- R/Stage2PSM.R
+++ R/Stage2PSM.R
@@ -3,7 +3,7 @@
 #These functions are to make common modeling strategies easier to specify for users
 #That do not write their own models.
 
-Stage2PSM <- function(model, dta, type, std_out = NULL)
+Stage2PSM <- function(model, dta, type, table_out = NULL, opts = NULL)
 {
   if(type == "lm")
   {
@@ -12,7 +12,7 @@ Stage2PSM <- function(model, dta, type, std_out = NULL)
     print.htmlTable(mTab)
     texreg::plotreg(m_fit,omit.coef="(match)|(Intercept)",custom.model.names="Unstandardized Model",custom.note=model)
     
-    if(!is.null(std_out))
+    if(!is.null(table_out))
     {
       dta_tmp <- dta
       d_index <- sapply(dta_tmp@data, is.numeric)
@@ -20,6 +20,26 @@ Stage2PSM <- function(model, dta, type, std_out = NULL)
       dta_fit_std <- lm(model,dta_tmp)
       texreg::plotreg(dta_fit_std,omit.coef="(match)|(Intercept)",custom.model.names="Standardized Model", custom.note=model)
     }
+    
+  }
+  
+  if(type == "cmreg")
+  {
+    m_fit <- lm(model,dta)
+    mTab <- stargazer(m_fit,type="html",title="Unstandardized Model Results")
+    print.htmlTable(mTab)
+    texreg::plotreg(m_fit,omit.coef="(match)|(Intercept)",custom.model.names="Unstandardized Model",custom.note=model)
+    
+    if(!is.null(table_out))
+    {
+      dta_tmp <- dta
+      d_index <- sapply(dta_tmp@data, is.numeric)
+      dta_tmp@data[d_index] <- lapply(dta_tmp@data[d_index],scale)
+      dta_fit_std <- lm(model,dta_tmp)
+      texreg::plotreg(dta_fit_std,omit.coef="(match)|(Intercept)",custom.model.names="Standardized Model", custom.note=model)
+    }
+    
+    
     
   }
 }

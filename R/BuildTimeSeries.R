a9666f66afa5d60535b2a7e2f22318aea9fa357a
DanRunfola <dan@danrunfola.com>
2015-05-05 15:30:02 -0400

    Cleaning melts up

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -1,12 +1,21 @@
 BuildTimeSeries <- function(dta,idField,varList_pre)
 {
-  grep_str = idField
+  #Run the melts
+  meltList <- list()
   for (i in 1:length(varList_pre))
   {
-    grep_str = paste(grep_str,varList_pre[i],sep="|")
+    grep_str = paste(idField,varList_pre[i],sep="|")
+    tDF <- dta@data[grepl(grep_str,names(dta@data))]
+    meltList[[i]] <- melt(tDF,id=idField)
   }
-  print(grep_str)
-  tDF <- dta@data[grepl(grep_str,names(dta@data))]
-  retDF <- melt(tDF,id=c(idField))
+
+  #Merge the melts together
+  retDF <- merge(meltList)
   return(retDF)
 }
+
+
+# dm1 <- melt(d[,c("Type","I.alt","idx06","idx07","idx08")], id=c("Type","I.alt"))
+# dm2 <- melt(d[,c("Type","I.alt","farve1","farve2")], id=c("Type","I.alt"))
+# colnames(dm2) <- c("Type", "I.alt", "variable2", "value2")
+# dm <- merge(dm1, dm2)

521e3aca71ad2c83c5d908b964c1533d51ae68f3
DanRunfola <dan@danrunfola.com>
2015-05-05 15:23:48 -0400

    debugging

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -5,7 +5,7 @@ BuildTimeSeries <- function(dta,idField,varList_pre)
   {
     grep_str = paste(grep_str,varList_pre[i],sep="|")
   }
-
+  print(grep_str)
   tDF <- dta@data[grepl(grep_str,names(dta@data))]
   retDF <- melt(tDF,id=c(idField))
   return(retDF)

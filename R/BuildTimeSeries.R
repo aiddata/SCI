9e5a4a86a8e39c3f1a4c34c97634ef4875aa0880
DanRunfola <dan@danrunfola.com>
2015-05-06 16:53:08 -0400

    test

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -11,6 +11,8 @@ BuildTimeSeries <- function(dta,idField,varList_pre,startYear,endYear,TrtYears=N
       varN <- paste("TrtMnt",years[k],sep="")
       exec <- paste("dta$",varN,"=0",sep="")
       eval(parse(text=exec))
+      return(dta)
+      break
       dta[varN][dta[TrtYears] >= as.numeric(years[k])] <- 1
     }
     View(dta["TrtMnt1996"])

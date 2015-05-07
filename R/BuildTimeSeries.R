d3443a50cc96457384f15ca81e840babe20c5660
DanRunfola <dan@danrunfola.com>
2015-05-07 10:28:17 -0400

    retbreak

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -61,7 +61,8 @@ BuildTimeSeries <- function(dta,idField,varList_pre,startYear,endYear,colYears=N
         #Melt the dataframe for modeling
         interpFrame[idField] <- row.names(interpFrame)
         melt_Model_dta <- melt(interpFrame,id=idField)
-        View(melt_Model_dta["variable"])
+        return(melt_Model_dta["variable"])
+        break
         melt_Model_dta["variable"] <- as.numeric(melt_Model_dta["variable"])
         #Fit the model for interpolation
         mdl <- lm(value ~ variable + factor(reu_id),data=melt_Model_dta)

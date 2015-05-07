7cdc2febdb5be39298b3c50a1679bde8e9edbe77
DanRunfola <dan@danrunfola.com>
2015-05-07 12:07:28 -0400

    s

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -65,7 +65,9 @@ BuildTimeSeries <- function(dta,idField,varList_pre,startYear,endYear,colYears=N
         View(interpFrame)
         melt_Model_dta <- melt(data.frame(interpFrame),id=idField)
         View(melt_Model_dta)
-        melt_Model_dta["variable"] <- as.numeric(gsub("X","",melt_Model_dta$variable)
+        melt_Model_dta["variable"] <- as.numeric(gsub("X","",melt_Model_dta$variable))
+        View(melt_Model_dta)
+                                                 
         #Fit the model for interpolation
         mdl <- lm(value ~ variable + factor(reu_id),data=melt_Model_dta)
         #Apply the model to interpolate

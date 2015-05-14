b1dc8d697db69babe0de520e6e6fbb2bac17eb13
DanRunfola <dan@danrunfola.com>
2015-05-14 14:09:17 -0400

    testing no DF correction and HC3 vcov

--- R/Stage2PSM.R
+++ R/Stage2PSM.R
@@ -72,7 +72,7 @@ Stage2PSM <- function(model, dta, type, table_out = NULL, opts = NULL)
     }
     
     print(opts)
-    exec = paste("cluster.vcov(m_fit,cbind(dta$",opts[1],",dta$",opts[2],"),leverage=3)",sep="")
+    exec = paste("cluster.vcov(m_fit,cbind(dta$",opts[1],",dta$",opts[2],"),leverage=3,df_correction=FALSE)",sep="")
     m_fit$var <- eval(parse(text=exec))
     CMREG <- coeftest(m_fit,m_fit$var)
     print("cmReg:")

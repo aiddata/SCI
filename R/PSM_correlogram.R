b84aae6f849c1a306e618a2c1bf922fd554bb6ae
DanRunfola <dan@danrunfola.com>
2015-07-20 12:00:31 -0400

    library namespace issues

--- R/PSM_correlogram.R
+++ R/PSM_correlogram.R
@@ -3,6 +3,7 @@
 PSM_correlogram <- function (dta, var, order = 1, style = "W", 
           randomisation = TRUE, zero.policy = NULL, spChk = NULL, start,end) 
 {
+  library(spdep)
   stopifnot(is.vector(var))
   if (any(is.na(var))) 
     stop("no NAs permitted in variable")

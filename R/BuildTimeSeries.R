2627231ecb689c3d0b8a6420d61035c845e037f2
userz <sgoodman@aiddata.org>
2015-09-15 13:55:53 -0400

    Test.

--- R/BuildTimeSeries.R
+++ R/BuildTimeSeries.R
@@ -5,9 +5,7 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
 
 
 
-
     print("bts1")
-    timer <- proc.time()
 
     # If there is a "colYears" variable, convert to binaries.
     # Eventually could be extended to more than one column.
@@ -15,40 +13,20 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
         # For each variable, for each year, create a binary representing the treatment status.
         for (j in 1:length(colYears)) {
 
-            # dta@data[colYears[j]] <- lapply(dta@data[colYears[j]], as.Date)
-
             for (k in 1:length(years)) {
 
-                varN <- paste("TrtMnt_",colYears[j],"_",years[k], sep="")
+                varN = paste("TrtMnt_",colYears[j],"_",years[k], sep="")
                 print(varN)
-
-                # exec <- paste("dta$",varN," = 0", sep="")
-                # eval(parse(text=exec))
-
-                # dta@data[[varN]] <- lapply(dta@data[colYears[j]], function (colYear_date) {
-                #     if (colYear_date <= as.Date(paste(years[k],"01","01", sep="-"))) {
-                #         return(1)
-                #     } else {
-                #         return(0)
-                #     }
-                # })
 
                 dta@data[[varN]] <- 0
                 dta@data[[varN]][dta@data[[colYears[j]]] <= as.Date(paste(years[k],"01","01", sep="-"))] <- 1
 
             }
+
+            # add the "TrtMnt_" + colYears[j] prefix to interpYears
+            trt_id = paste("TrtMnt_",colYears[j],"_####", sep="")
+            interpYears <- c(interpYears, trt_id)    
         }
-    }
-
-    return(dta)
-
-    print(colnames(dta@data))
-
-
-    # add the "TrtMnt_" + colYears[j] prefix to interpYears
-    for (j in 1:length(colYears)) {
-        trt_id = paste("TrtMnt_",colYears[j],"_####", sep="")
-        interpYears <- c(interpYears, trt_id)  
     }
 
     print(interpYears)
@@ -58,24 +36,29 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
 
 
 
-    print("bts3")
+    print("bts2")
     timer <- proc.time()
     
     # If there is an "interpVars" variable, linearly interpolate values based on at least 2 known points in time.
     if (!is.null(interpYears)) {
 
-        print("bts3.0")
-
         for (AncInt in 1:length(interpYears)) {
 
             print(interpYears[AncInt])
 
+
             cur_ancVi <- interpYears[AncInt]
+
+            # create interpolation data frame and add id field
             interpFrame <- dta@data[idField]
             interpFrame[idField] <- dta@data[idField]
+            
             cnt = 2
 
-            print("bts3.0.1")
+
+            print("bts2.0.1")
+
+            # add data to interpolation data frame
             if (cur_ancVi %in% colnames(dta@data)) {
                     # Exception for a single-point interpolation
                     interpFrame[cnt] <- dta@data[[cur_ancVi]]
@@ -93,45 +76,47 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
                         colnames(interpFrame)[cnt] <- years[[k]]
                         cnt = cnt + 1
 
-                    } 
+                    }
                 }
             }
 
             print(cnt)
 
 
-            print("bts3.0.2")
-            # this is a slow part
+            print("bts2.0.2")
 
-            # Only one time point, so no interpolation is done - value is simply copied to all other columns.
             if (cnt == 3) {
-                print("bts3.0.2a")
+                # only one time point, so no interpolation is done - value is simply copied to all other columns.
+
+                print("bts2.0.2a")
                 for (k in 1:length(years)) {
                     # add _year to end of non temporal data
                     dta@data[[paste(cur_ancVi,years[[k]],sep="_")]] <- interpFrame[2]
                 }
 
             } else if (cnt < length(years) + 2) {
-                print("bts3.0.2b0")
-                tDframe <- dta@data[idField]
-
-                # Here, we model out everything. 
+                # run model if data exists for at least two years but not for all years
+                print("bts2.0.2b0")
 
                 # Melt the dataframe for modeling
                 melt_Model_dta <- melt(data.frame(interpFrame), id=idField)
                 melt_Model_dta["variable"] <- as.numeric(gsub("X", "", melt_Model_dta[["variable"]]))
                 
 
+                print("bts2.0.2b1")
+
                 # Fit the model for interpolation
-                print("bts3.0.2b1")
-                
+                # this is a slow part
                 execstr <- paste("mdl <- lm(value ~ variable + factor(",idField,"),data=melt_Model_dta)", sep="")
                 eval(parse(text=execstr))
                 # mdl <- lm(value ~ variable + factor(idField), data=melt_Model_dta)
 
-                print("bts3.0.2b2")
+
+                print("bts2.0.2b2")
 
                 # Apply the model to interpolate
+                tDframe <- dta@data[idField]
+
                 for (u in 1:length(years)) {
 
                     varI <- gsub('####', years[[u]], cur_ancVi)
@@ -147,7 +132,7 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
 
         }
 
-        print("bts3.1")        
+        print("bts2.1")        
         # Append interpolated fields to our melting lists
         varList_pre <- c(varList_pre, interpYears)
     
@@ -159,17 +144,17 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
     print(paste("section completed in", timer[3], "seconds."))
 
 
+    return(dta)
 
 
-
-    print("bts4")
+    print("bts3")
     timer <- proc.time()
 
     # Run the melts
     meltList <- list()
     for (i in 1:length(varList_pre)) {
 
-        print("bts4.0")
+        print("bts3.0")
         print(varList_pre[[i]])
 
         # Limit to only relevant years
@@ -186,28 +171,28 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
         }
         print(grepStrYrs)
 
-        print("bts4.1")
+        print("bts3.1")
         print(names(dta@data))
 
         tDF <- dta@data[grepl(grepStrYrs, names(dta@data))]
         meltList[[i]] <- melt(tDF, id=idField)
         
-        print("bts4.2")
+        print("bts3.2")
         print(colnames(meltList[[i]]))
         # Keep only years in the year column, rename columns
         colnames(meltList[[i]])[2] <- "Year"
 
-        print("bts4.3")
+        print("bts3.3")
 
         colnames(meltList[[i]])[3] <- varList_pre[[i]]
 
-        print("bts4.4")
+        print("bts3.4")
 
         # Clean up year column
         gsub_command <- paste("^",varList_pre[[i]],sep="")
         meltList[[i]][2] <- gsub(gsub_command, "", as.matrix(meltList[[i]][2]))
         
-        print("bts4.5")
+        print("bts3.5")
 
         # Remove ID and year if this is at least the second variable to avoid duplications.
         if (i > 1) {
@@ -223,7 +208,7 @@ BuildTimeSeries <- function (dta, idField, varList_pre, startYear, endYear, colY
 
 
 
-    print("bts5")
+    print("bts4")
 
     # Finish up with a cherry on top
     meltListRet <- data.frame(meltList)

5303f2dae1394151312e77cf1546c9cbeb2ce0a2
DanRunfola <dan@danrunfola.com>
2015-09-15 16:06:16 -0400

    Final TS Commit

--- R/TimeSeriesLag.R
+++ R/TimeSeriesLag.R
@@ -1,10 +1,11 @@
-TimeSeriesLag <- function(dta,yearID,unit_ID,lagNum,inName,outName,start_yr,end_yr)
+TimeSeriesLag<-function (dta, yearID, unit_ID, lagNum, inName, outName, start_yr, end_yr) 
 {
-  for(i in 1:length(dta[[1]]))
+  dta[outName] <- NA
+  for (i in 1:length(dta[[1]])) 
   {
-  cur_id = dta[unit_ID][i,]
-  cur_year = dta[yearID][i,]
-  dta[outName][i,] <- dta[dta[yearID] == (cur_year-lagNum) & dta[unit_ID] == cur_id,][inName][1,]
+    cur_id = dta[unit_ID][i, ]
+    cur_year = dta[yearID][i,]
+    dta[outName][i,] <- dta[dta[yearID] == (cur_year-lagNum) & dta[unit_ID]==cur_id,][inName][1,]
   }
   return(dta)
 }

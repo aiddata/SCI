11346a7c71a07a3d78c757e642d8f3301bde9791
DanRunfola <dan@danrunfola.com>
2015-09-15 15:36:16 -0400

    test

--- R/TimeSeriesLag.R
+++ R/TimeSeriesLag.R
@@ -6,8 +6,9 @@ TimeSeriesLag <- function(dta,yearID,unit_ID,lagNum,inName,outName,start_yr,end_
     cur_year = start_yr
     while(cur_year <= end_yr)
     {
-      dta[outName][i,] <- dta[inName][which(data[yearID] == cur_year-lagNum && data[unit_ID] == cur_id)]
+      dta[outName][i,] <- dta[[inName]][which(data[yearID] == cur_year-lagNum && data[unit_ID] == cur_id)]
       cur_year = cur_year + 1
     }
   }
+  return(dta)
 }

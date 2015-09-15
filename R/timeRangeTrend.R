76ea9b3c6b8b017200971fe55eede28cca6ada12
userz <sgoodman@aiddata.org>
2015-09-15 11:09:10 -0400

    Test.

--- R/timeRangeTrend.R
+++ R/timeRangeTrend.R
@@ -2,27 +2,27 @@
 # by field prefix and return coefficients
 timeRangeTrend <- function(dta, prefix, startyr, endyr, IDfield, thresh=0.5) {
 
-    # create new dataframe from all columns in dta dataframe that 
-    # are either the ID or a year which is indicated by the prefix
-    grep_str = paste(IDfield, prefix, sep="|")
-    tDF <- dta@data[grepl(grep_str, names(dta@data))]
+    # # create new dataframe from all columns in dta dataframe that 
+    # # are either the ID or a year which is indicated by the prefix
+    # grep_str = paste(IDfield, prefix, sep="|")
+    # tDF <- dta@data[grepl(grep_str, names(dta@data))]
 
-    # melt all years columns in new dataframe
-    analysisDF <- melt(tDF, id=c(IDfield))
+    # # melt all years columns in new dataframe
+    # analysisDF <- melt(tDF, id=c(IDfield))
     
-    # cleaned GREP - remove year digit placeholders
-    # new_pre <- gsub("[0-9]", "", prefix, fixed=TRUE)
+    # # cleaned GREP - remove year digit placeholders
+    # # new_pre <- gsub("[0-9]", "", prefix, fixed=TRUE)
 
-    # get location of year in prefix
-    yIndex <- regexpr("[0-9]", prefix, fixed=TRUE)
+    # # get location of year in prefix
+    # yIndex <- regexpr("[0-9]", prefix, fixed=TRUE)
 
-    # generate new year field by removing prefix from variable (original column names)
-    # analysisDF["Year"] <- lapply(analysisDF["variable"], FUN=function(x) as.numeric(gsub(new_pre, "", x)))
-    analysisDF["Year"] <- lapply(analysisDF["variable"], FUN=function(x) as.numeric(substr(x, yIndex[1], yIndex[1]+3)))
+    # # generate new year field by removing prefix from variable (original column names)
+    # # analysisDF["Year"] <- lapply(analysisDF["variable"], FUN=function(x) as.numeric(gsub(new_pre, "", x)))
+    # analysisDF["Year"] <- lapply(analysisDF["variable"], FUN=function(x) as.numeric(substr(x, yIndex[1], yIndex[1]+3)))
 
-    # keep years in range specified
-    analysisDF <- analysisDF[analysisDF["Year"] >= startyr ,]
-    analysisDF <- analysisDF[analysisDF["Year"] <= endyr ,]
+    # # keep years in range specified
+    # analysisDF <- analysisDF[analysisDF["Year"] >= startyr ,]
+    # analysisDF <- analysisDF[analysisDF["Year"] <= endyr ,]
 
     # # create empty field
     # dta@data["newfieldID"] <- 0

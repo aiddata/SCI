f21621b6d5195d9bca970a129e897a39109ae774
userz <sgoodman@aiddata.org>
2015-10-19 12:20:01 -0400

    Test

--- R/fastNN_binary_func.R
+++ R/fastNN_binary_func.R
@@ -102,23 +102,18 @@ fastNN_binary_func <- function(dta, trtMntVar, ids, curgrp, dist_PSM) {
         #This will give us the matched index in the "treated" dataset.
         best_m_treated = k[["nn.index"]][best_m_control]
         
-        # #Control PSM ID
-        # Control_ID = toString(untreated[[ids]][[best_m_control]])    
 
-        # #Treatment PSM ID
-        # Treatment_ID = toString(treated[[ids]][[best_m_treated]])
-   
-        # #Control PSM ID
-        # cid_txt = paste("untreated$",ids,"[",best_m_control,"]",sep="")
-        # Control_ID = toString(eval(parse(text=cid_txt)))
+        #Control PSM ID
+        cid_txt = paste("untreated$",ids,"[",best_m_control,"]",sep="")
+        Control_ID = toString(eval(parse(text=cid_txt)))
             
-        # #Treatment PSM ID
-        # tid_txt = paste("treated$",ids,"[",best_m_treated,"]",sep="")
-        # Treatment_ID = toString(eval(parse(text=tid_txt)))
+        #Treatment PSM ID
+        tid_txt = paste("treated$",ids,"[",best_m_treated,"]",sep="")
+        Treatment_ID = toString(eval(parse(text=tid_txt)))
 
         #Control PSM ID and Treatment PSM ID
-        Control_ID = toString(untreated[,ids][best_m_control])
-        Treatment_ID = toString(treated[,ids][best_m_treated])
+        # Control_ID = toString(untreated[,ids][best_m_control])
+        # Treatment_ID = toString(treated[,ids][best_m_treated])
 
 
         #Create a unique pair ID for each group (will simply append a "1" if only 1 group)
